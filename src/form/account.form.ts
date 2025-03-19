dd738d44865df0795503c9a0e8f6819288ae928f
Micha Oberstein <Moberstein@users.noreply.github.com>
2025-03-19 15:36:44 +0100

    feat: prepare updated typescript model

--- src/form/account.form.ts
+++ src/form/account.form.ts
@@ -1,5 +1,51 @@
 export namespace Account {
-    export function onLoad(executionContext: Xrm.Events.EventContext) {
+    export function onLoadWithEarlyBoundSpecificForm(executionContext: Xrm.Events.EventContext) {
+        // Get a specific form context containing only the attributes that exists on this form
+        const formContext = executionContext.getFormContext<XrmTable.Account.AccountMainFormContext>();
 
+        // Get inline hints for the attibute names that do exist on the form
+        const nameAttribute = formContext.getAttribute("name");
+
+        // Same goes for the tabs/sections/controls
+        const nameControl = formContext.ui.tabs.get("SUMMARY_TAB").sections.get("ACCOUNT_INFORMATION").controls.get("name");
+
+        if (nameControl.getAttribute() == nameAttribute)
+        {
+            // Do something
+        }
+    }
+
+    export function onLoadWithEarlyBoundGeneral(executionContext: Xrm.Events.EventContext) {
+        // Get a form context containing all the attibutes of the table
+        const formContext = executionContext.getFormContext<XrmTable.Account.FormContext>();
+
+        // Get inline hints for the attibute names that do exist on the form
+        const nameAttribute = formContext.getAttribute("name");
+
+        // Same goes for the tabs/sections/controls
+        const nameControl = formContext.ui.tabs.get("SUMMARY_TAB").sections.get("ACCOUNT_INFORMATION").controls.get("name");
+
+        // See how we need to cast the control since we don't know the specific type?
+        if ((<Xrm.Controls.StringControl>nameControl).getAttribute() == nameAttribute)
+        {
+            // Do something
+        }
+    }
+
+    export function onLoad(executionContext: Xrm.Events.EventContext) {
+        // Get a generic form context without any type hints
+        const formContext = executionContext.getFormContext();
+
+        // A wrong name for the get attribute method will not be caught as error here
+        const nameAttribute = formContext.getAttribute("name");
+
+        // Same goes for the tabs/sections/controls
+        const nameControl = formContext.ui.tabs.get("SUMMARY_TAB").sections.get("ACCOUNT_INFORMATION").controls.get("name");
+
+        // See how we need to cast the control since we don't know the specific type?
+        if ((<Xrm.Controls.StringControl>nameControl).getAttribute() == nameAttribute)
+        {
+            // Do something
+        }
     }
 }
